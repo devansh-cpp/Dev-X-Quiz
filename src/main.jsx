9acb14a2f1c6c589919571f3a2805f809cb0af03
devansh-cpp <113198313+devansh-cpp@users.noreply.github.com>
2024-10-20 11:11:32 +0530

    disabling the click

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.addEventListener('keydown', (e) => {
+  if (e.key.startsWith('F') || e.key === 'F5') {
+    e.preventDefault();
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>
     <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-    <App />
+      <App />
     </div>
-    </div>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
